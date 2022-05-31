347463821a76d3e7beff39df1ac1db5a0af7af70
Pablo Caballero <deidigitalis@gmail.com>
2022-06-01 00:04:31 +0200

    Rectoring + documentation.

--- AbstractBuilder/RecordBuilder.cs
+++ AbstractBuilder/RecordBuilder.cs
@@ -7,29 +7,61 @@ namespace AbstractBuilder
     using System.Reflection;
     using AbstractBuilder.Internal;
 
+    /// <summary>
+    /// Builder for records.
+    /// </summary>
+    /// <typeparam name="TResult">Type of the result of this builder</typeparam>
     public class RecordBuilder<TResult>
     {
         private readonly IDictionary<string, Func<object>> _propertyBuilders = new Dictionary<string, Func<object>>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RecordBuilder{TResult}"/> class.
+        /// </summary>
         protected RecordBuilder()
         {
         }
 
-        public TBuilder Set<TBuilder>(Expression<Func<TResult, object>> selector, Func<object> propertyBuilder)
+        /// <summary>
+        /// Attaches new parameter construction in a new director.
+        /// </summary>
+        /// <typeparam name="TBuilder">Type of the builder</typeparam>
+        /// <param name="selector">Selects the parameter name to be initilized</param>
+        /// <param name="parameterBuilder">Parameter builder</param>
+        /// <returns>An incremental new director</returns>
+        /// <exception cref="MissingFieldException">When the parameter does not exist</exception>
+        public TBuilder Set<TBuilder>(Expression<Func<TResult, object>> selector, Func<object> parameterBuilder)
             where TBuilder : RecordBuilder<TResult>
         {
             var propertyInfo = (PropertyInfo)((MemberExpression)selector.Body).Member;
-            return Set<TBuilder>(propertyInfo.Name, () => propertyBuilder.Invoke());
+            return Set<TBuilder>(propertyInfo.Name, () => parameterBuilder.Invoke());
         }
 
-        public TBuilder Set<TBuilder, TProperty>(Expression<Func<TResult, TProperty>> selector, Func<TProperty> propertyBuilder)
+        /// <summary>
+        /// Attaches new parameter construction in a new director.
+        /// </summary>
+        /// <typeparam name="TBuilder">Type of the builder</typeparam>
+        /// <typeparam name="TParameter">Type of the parameter</typeparam>
+        /// <param name="selector">Selects the parameter name to be initilized</param>
+        /// <param name="parameterBuilder"></param>
+        /// <returns>An incremental new director</returns>
+        /// <exception cref="MissingFieldException">When the parameter does not exist</exception>
+        public TBuilder Set<TBuilder, TParameter>(Expression<Func<TResult, TParameter>> selector, Func<TParameter> parameterBuilder)
             where TBuilder : RecordBuilder<TResult>
         {
             var propertyInfo = (PropertyInfo)((MemberExpression)selector.Body).Member;
-            return Set<TBuilder>(propertyInfo.Name, () => propertyBuilder.Invoke());
+            return Set<TBuilder>(propertyInfo.Name, () => parameterBuilder.Invoke());
         }
 
-        public TBuilder Set<TBuilder>(string propertyName, Func<object> propertyBuilder)
+        /// <summary>
+        /// Attaches new parameter construction in a new director.
+        /// </summary>
+        /// <typeparam name="TBuilder">Type of the builder</typeparam>
+        /// <param name="parameterName"></param>
+        /// <param name="parameterBuilder"></param>
+        /// <returns>An incremental new director</returns>
+        /// <exception cref="MissingFieldException">When the parameter does not exist</exception>
+        public TBuilder Set<TBuilder>(string parameterName, Func<object> parameterBuilder)
             where TBuilder : RecordBuilder<TResult>
         {
             if (!IsSupported<TBuilder>())
@@ -37,22 +69,27 @@ namespace AbstractBuilder
                 throw new NotSupportedException();
             }
 
-            if (!ExistParameter(propertyName))
+            if (!ExistParameter(parameterName))
             {
-                throw new MissingFieldException(typeof(TResult).Name, propertyName);
+                throw new MissingFieldException(typeof(TResult).Name, parameterName);
             }
 
             RecordBuilder<TResult> builder = CreateBuilder();
 
-            foreach (var propBuilders in _propertyBuilders.Where(x => x.Key != propertyName))
+            foreach (var propBuilders in _propertyBuilders.Where(x => x.Key != parameterName))
             {
                 builder._propertyBuilders.Add(propBuilders.Key, propBuilders.Value);
             }
-            builder._propertyBuilders.Add(propertyName, () => propertyBuilder.Invoke());
+            builder._propertyBuilders.Add(parameterName, () => parameterBuilder.Invoke());
 
             return (TBuilder)builder;
         }
 
+        /// <summary>
+        /// Builds a new instance
+        /// </summary>
+        /// <returns>A new object</returns>
+        /// <exception cref="MissingMethodException">When the constructor is not available</exception>
         public TResult Build()
         {
             ConstructorInfo ctor = typeof(TResult).GetConstructors().FirstOrDefault()
@@ -65,26 +102,35 @@ namespace AbstractBuilder
             return (TResult)ctor.Invoke(parameters.ToArray());
         }
 
+        /// <summary>
+        /// Build the given parameter
+        /// </summary>
+        /// <param name="parameter">Parameter info</param>
+        /// <returns>The value of the parameter for the new object</returns>
         private object BuildParameter(ParameterInfo parameter)
         {
-            if (_propertyBuilders.TryGetValue(parameter.Name, out Func<object> propertyBuilder))
+            if (_propertyBuilders.TryGetValue(parameter.Name, out Func<object> parameterBuilder))
             {
-                return propertyBuilder.Invoke();
+                return parameterBuilder.Invoke();
             }
-            else if (parameter.HasDefaultValue)
+
+            if (parameter.HasDefaultValue)
             {
                 return parameter.DefaultValue;
             }
-            else if (parameter.ParameterType.IsClass)
+
+            if (parameter.ParameterType.IsClass)
             {
                 return null;
             }
-            else
-            {
-                return Activator.CreateInstance(parameter.ParameterType);
-            }
+
+            return Activator.CreateInstance(parameter.ParameterType);
         }
 
+        /// <summary>
+        /// Creates a new empty builder
+        /// </summary>
+        /// <exception cref="MissingMethodException">When the constructor is not available</exception>
         private RecordBuilder<TResult> CreateBuilder()
         {
             var type = GetType();
@@ -107,6 +153,11 @@ namespace AbstractBuilder
             return resultType == currentType || resultType.IsInstanceOfType(currentType);
         }
 
+        /// <summary>
+        /// Checks the existence of a parameter in the first found constructor.
+        /// </summary>
+        /// <param name="name">Parameter name</param>
+        /// <returns>true if it was found, otherwise false</returns>
         private static bool ExistParameter(string name)
         {
             return typeof(TResult).GetConstructors().FirstOrDefault()?.GetParameters().Any(p => p.Name == name) ?? false;
